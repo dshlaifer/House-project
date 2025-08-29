e2d1c145eaa7bfd9b948fde1ec0143a227a60e96
David Shlaifer <dshlaifer@gmail.com>
2025-08-29 15:32:56 -0400

    readme updated and code

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        sun2 = new Circle();
         drawn = false;
     }
 
@@ -51,11 +53,17 @@ public class Picture
             roof.moveVertical(-60);
             roof.makeVisible();
     
-            sun.changeColor("yellow");
+            sun.changeColor("blue");
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
+            
+            sun2.changeColor("yellow");
+            sun2.moveHorizontal(-140);
+            sun2.moveVertical(-50);
+            sun2.changeSize(80);
+            sun2.makeVisible();
             drawn = true;
         }
     }
